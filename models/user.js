2449a821a65fa0c79619ad93dd12713d20a01973
unknown <german280105@mail.ru>
2023-04-12 14:47:16 +0300

    создал схемы user и movie

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
