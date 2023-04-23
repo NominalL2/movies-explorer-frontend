5f2f7b31d32a3a864ea8340c7f952c2737e9d900
unknown <german280105@mail.ru>
2023-04-23 17:58:44 +0300

    Сверстал Promo

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import './Main.css';
 
 import NavTab from './NavTab/NavTab.js';
+import Promo from './Promo/Promo.js';
 
 function Main() {
   return (
     <main className='content'>
       <NavTab></NavTab>
+      <Promo></Promo>
     </main>
   );
 }
---
+++ src/components/Main/Promo/Promo.js
@@ -0,0 +1,31 @@
+import './Promo.css';
+
+function Promo() {
+  return (
+    <>
+      <section className='promo'>
+        <h2 className='promo__title'>О проекте</h2>
+        <div className='promo__info'>
+          <div className='promo__section'>
+            <h3 className='promo__title-info'>Дипломный проект включал 5 этапов</h3>
+            <p className='promo__text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          </div>
+          <div className='promo__section'>
+            <h3 className='promo__title-info'>На выполнение диплома ушло 5 недель</h3>
+            <p className='promo__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+          </div>
+        </div>
+        <div className='promo__line'>
+          <div className='promo__backend'>1 неделя</div>
+          <div className='promo__frontend'>4 недели</div>
+        </div>
+        <div className='promo__captions'>
+          <p className='promo__caption-backend'>Back-end</p>
+          <p className='promo__caption-frontend'>Front-end</p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Promo;
