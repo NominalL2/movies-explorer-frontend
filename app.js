a32dae1122194953f50b94cfa2cb355daaac60f3
unknown <german280105@mail.ru>
2023-05-07 15:18:28 +0300

    Исправил ошибку с cors

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ mongoose.connect(MONGO_DB, {
   useNewUrlParser: true,
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(requestLogger);
 
 app.use('/', require('./routes/index'));
